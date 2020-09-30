ba06acad00ee627b1d600dddc68c7e7125f68f6f
Masaoud Khallouf <masaoudkhallouf@Masaouds-MBP.fritz.box>
2020-09-30 16:45:04 +0200

    add numbering system file

---
+++ numeringSystem.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	//num in Decimal
+	fmt.Println(42)
+
+	//num in Binary
+	fmt.Printf("%d - %b \n", 42, 42)
+
+	//num in hexadecimal
+	fmt.Printf("%x\n", 42)
+	fmt.Printf("%#x\n", 42)
+	fmt.Printf("%#X\n", 42)
+
+	//loop
+	for i:=0; i <= 100; i++{
+		fmt.Printf("%d \t %b \t %x \n",i, i, i)
+	}
+}
